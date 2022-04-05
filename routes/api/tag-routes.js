7bdf2cb4fe7c049ea1d05d9791d54b8262af7c08
Lexi Dorrity <lexidorrity@Lexis-MBP.socal.rr.com>
2022-04-05 14:14:47 -0700

    worked on routes, finished category routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -11,14 +11,32 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findAll({
+      include: [{ model: ProductTag, through:Tag , as: 'tagged_product' }],
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+    try {
+        const tagData = await Tag.create({
+          tag_id: req.body.tag_id,
+        });
+        res.status(200).json(tagData);
+      } catch (err) {
+        res.status(400).json(err);
+      }
+  
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+
 });
 
 router.delete('/:id', (req, res) => {
